7b562b31060a8908bbcbc27402ce9ab8b06220a1
Antonyhz <hernandezantony333@gmail.com>
2025-11-15 18:49:57 -0400

    agregando ventana modal para agregar gasto al budget

--- control-gastos-contextAPI/src/reducers/budget-reducer.ts
+++ control-gastos-contextAPI/src/reducers/budget-reducer.ts
@@ -1,15 +1,19 @@
 
 
 export type BudgetActions = 
-    { type: 'add-budget', payload: {budget: number} };
+    { type: 'add-budget', payload: {budget: number}} |
+    {type: 'show-modal'} |
+    {type: 'close-modal'};
 
 
 export type BudgetState = {
     budget: number,
+    modal: boolean
 };
 
 export const initialState : BudgetState = {
     budget: 0,
+    modal: false
 }
 
 export const budgetReducer = (
@@ -26,6 +30,20 @@ export const budgetReducer = (
         return{
             ...state,
             budget
+        }
+    }
+
+    if(action.type === 'show-modal'){
+        return{
+            ...state,
+            modal: true
+        }
+    }
+
+    if(action.type === 'close-modal'){
+        return{
+            ...state,
+            modal: false
         }
     }
 
